aae89d765cbd378f7392d8509bbfdbea33f38c04
Martin <momartelo@yahoo.com.ar>
2024-05-11 00:07:04 -0300

    cambios

--- js/body-wishlist.js
+++ js/body-wishlist.js
@@ -33,22 +33,29 @@ if (wishlistItems.length > 0) {
   for (i = 0; i < wishlistItems.length; i++) {
     wishCad += `
     <div class="wishlist-container flex">
-            <article class="article-product flex">
-                        <a href="#" onclick="event.preventDefault();">
-                            <picture>
-                                <img
-                                src="${wishlistItems[i].image}"
-                                alt=""
-                                />
-                            </picture>
-                        </a>
-                        <div class="product-description">
-                            <span class="description">${wishlistItems[i].productName}</span>
-                        </div>
-                        <div class="product-price">
-                            <span class="price">${wishlistItems[i].prize}</span>
-                        </div>
-            </article>
+      <article class="article-product flex">
+        <a href="#" onclick="event.preventDefault();">
+          <picture>
+            <img
+            src="${wishlistItems[i].image}"
+            alt=""
+            />
+          </picture>
+          <button class="wishlist-button-add flex">
+          <img
+            src="./img/corazonRojo3D.png"
+            alt=""
+            width="16px"
+          />
+        </button>
+        </a>
+        <div class="product-description">
+          <span class="description">${wishlistItems[i].productName}</span>
+        </div>
+        <div class="product-price">
+          <span class="price">${wishlistItems[i].prize}</span>
+        </div>
+      </article>
     </div>
 `;
 
@@ -74,7 +81,8 @@ if (total == 0) {
 `;
 }
 wishCad += `
-        <div class="cart-hidden-container-popular">
+      <div class="wishlist-container3">
+        <div class="cart-hidden-container-popular flex">
             <a href="#"
             ><img
             class="icono-cart-popular"
@@ -89,6 +97,7 @@ wishCad += `
         <div class="product-price">
             <span class="price">Total $${total.toFixed(2)}</span>
         </div>
+        
     </div>
 
 
@@ -102,4 +111,58 @@ document.getElementById("vaciarLista").addEventListener("click", function () {
   location.reload();
   // Actualizar la visualización o realizar cualquier otra acción necesaria
   console.log("La lista de deseos ha sido vaciada.");
+});
+
+//-------------------------------------------------------------------------//
+
+const wishlistButtons = document.querySelectorAll(".wishlist-button-add");
+console.log(wishlistButtons);
+
+function toggleWishlistButton(elemento) {
+  console.log(elemento);
+  const imageElement = elemento.querySelector("img");
+  const contenedorProducto = elemento.closest(".wishlist-container");
+  const nombreProducto = contenedorProducto
+    .querySelector("div span")
+    .textContent.trim();
+  console.log(nombreProducto);
+
+  let productoID = null;
+
+  for (var i = 0; i < data.length; i++) {
+    if (nombreProducto === data[i].nombre) {
+      productoID = data[i].id;
+      console.log(productoID);
+      break;
+    }
+  }
+
+  if (!productoID) {
+    console.error("Producto no encontrado en la lista de datos.");
+    return; // Sale de la función si el producto no se encuentra
+  }
+
+  const productIdToDelete = productoID;
+  console.log("codigo de producto " + productIdToDelete);
+  let wishlistItems = JSON.parse(localStorage.getItem("wishlist"));
+  console.log("wishlist" + wishlistItems);
+
+  if (wishlistItems) {
+    wishlistItems = wishlistItems.filter(
+      (item) => item.productId !== productIdToDelete
+    );
+    localStorage.setItem("wishlist", JSON.stringify(wishlistItems));
+    contenedorProducto.remove();
+    console.log("Producto eliminado de la lista de deseos");
+  } else {
+    console.log("No hay productos en la lista de deseos en el localStorage.");
+  }
+}
+
+wishlistButtons.forEach((elemento) => {
+  elemento.addEventListener("click", (event) => {
+    event.preventDefault();
+    toggleWishlistButton(elemento);
+    window.location.reload();
+  });
 });
